39def2e27a94eed8565d58a40a4c90f377499c88
Gabriel Alves <alvesg2003@gmail.com>
2022-02-07 21:15:40 -0300

    Cookies e Sessions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails - Gabriel Alves Cookies"
+    session[:curso] = "Curso de Ruby on Rails - Gabriel Alves Sessions"
     @nome = params[:nome]
+    @curso = params[:curso]
   end
 end
