4078cfedb6b9b52a15f8b7a9f6d13c4e5e22e9a9
Gabriel Alves <alvesg2003@gmail.com>
2022-02-12 12:59:01 -0300

    arrumando bugs

--- db/schema.rb
+++ db/schema.rb
@@ -1,32 +1,29 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+<p id="notice"><%= notice %></p>
 
-ActiveRecord::Schema.define(version: 2022_02_03_214227) do
+<h1><%= MiningType.model_name.human(count: @mining_types.count) %></h1>
 
-  create_table "coins", force: :cascade do |t|
-    t.string "description"
-    t.string "acronym"
-    t.string "url_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "mining_type_id"
-    t.index ["mining_type_id"], name: "index_coins_on_mining_type_id"
-  end
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th><%= @mining_types.human_attribute_name(:description) %></th>
+      <th><%= @mining_types.human_attribute_name(:acronym) %></th>
+      <th colspan="3"></th>
+    </tr>
+  </thead>
 
-  create_table "mining_types", force: :cascade do |t|
-    t.string "description"
-    t.string "acronym"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  <tbody>
+    <% @mining_types.each do |mining_type| %>
+      <tr>
+        <td><%= mining_type.description %></td>
+        <td><%= mining_type.acronym %></td>
+        <td><%= link_to t('links.show'), mining_type %></td>
+        <td><%= link_to t('links.edit'), edit_mining_type_path(mining_type) %></td>
+        <td><%= link_to t('links.delete'), mining_type, method: :delete, data: { confirm: t('messages.confirm') } %></td>
+      </tr>
+    <% end %>
+  </tbody>
+</table>
 
-end
+<br>
+
+<%= link_to t('new.mining_type'), new_mining_type_path, class:"btn btn-primary" %>
